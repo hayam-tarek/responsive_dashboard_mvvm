96d8194dc6d936c096abc4502606e246756c2e03
Hayam <hayamtareq28@gmail.com>
2026-05-15 22:03:09 +0300

    feat: add google_fonts dependency and implement AppTextStyle configuration

---
+++ lib/core/styles/app_text_style.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+
+  static TextStyle styleRegular12 = GoogleFonts.montserrat(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle styleMedium12 = GoogleFonts.montserrat(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleRegular14 = GoogleFonts.montserrat(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle styleMedium14 = GoogleFonts.montserrat(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleSemiBold14 = GoogleFonts.montserrat(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle styleRegular16 = GoogleFonts.montserrat(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle styleMedium16 = GoogleFonts.montserrat(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleSemiBold16 = GoogleFonts.montserrat(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle styleBold16 = GoogleFonts.montserrat(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle styleSemiBold18 = GoogleFonts.montserrat(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle styleMedium20 = GoogleFonts.montserrat(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleSemiBold20 = GoogleFonts.montserrat(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle styleBold20 = GoogleFonts.montserrat(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle styleSemiBold24 = GoogleFonts.montserrat(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+}
