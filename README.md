f41f565bf7c5cfb4fb381d4def8a2731b4b25a5c
Hayam <hayamtareq28@gmail.com>
2026-05-15 22:54:15 +0300

    feat: add SVG assets and integrate flutter_gen for type-safe asset access

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # responsive_dashboard_mvvm
 
 A new Flutter project.
+
+dart run build_runner build --delete-conflicting-outputs
---
+++ lib/core/gen/assets.gen.dart
@@ -0,0 +1,149 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $AssetsSvgsGen {
+  const $AssetsSvgsGen();
+
+  /// File path: assets/svgs/card-receive.svg
+  SvgGenImage get cardReceive =>
+      const SvgGenImage('assets/svgs/card-receive.svg');
+
+  /// File path: assets/svgs/card-send.svg
+  SvgGenImage get cardSend => const SvgGenImage('assets/svgs/card-send.svg');
+
+  /// File path: assets/svgs/category-2.svg
+  SvgGenImage get category2 => const SvgGenImage('assets/svgs/category-2.svg');
+
+  /// File path: assets/svgs/chart-2.svg
+  SvgGenImage get chart2 => const SvgGenImage('assets/svgs/chart-2.svg');
+
+  /// File path: assets/svgs/convert-card.svg
+  SvgGenImage get convertCard =>
+      const SvgGenImage('assets/svgs/convert-card.svg');
+
+  /// File path: assets/svgs/gallery.svg
+  SvgGenImage get gallery => const SvgGenImage('assets/svgs/gallery.svg');
+
+  /// File path: assets/svgs/graph.svg
+  SvgGenImage get graph => const SvgGenImage('assets/svgs/graph.svg');
+
+  /// File path: assets/svgs/logout.svg
+  SvgGenImage get logout => const SvgGenImage('assets/svgs/logout.svg');
+
+  /// File path: assets/svgs/moneys.svg
+  SvgGenImage get moneys => const SvgGenImage('assets/svgs/moneys.svg');
+
+  /// File path: assets/svgs/setting-2.svg
+  SvgGenImage get setting2 => const SvgGenImage('assets/svgs/setting-2.svg');
+
+  /// File path: assets/svgs/wallet-2.svg
+  SvgGenImage get wallet2 => const SvgGenImage('assets/svgs/wallet-2.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+    cardReceive,
+    cardSend,
+    category2,
+    chart2,
+    convertCard,
+    gallery,
+    graph,
+    logout,
+    moneys,
+    setting2,
+    wallet2,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsSvgsGen svgs = $AssetsSvgsGen();
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    _svg.ColorMapper? colorMapper,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+        colorMapper: colorMapper,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
